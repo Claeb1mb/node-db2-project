dff6de97d5785576b19f888e14c567a1e9b43e77
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2021-01-31 22:51:47 -0500

    retry migrate

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,16 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars-dealers.db3'
+      filename: './data/car-dealers.db3'
     },
-    useNullAsDefault
-  },
+    useNullAsDefault: true,
   //tells knex where to put the migrations folder
   migrations:{
     directory:'./data/migrations'
   },
+  //tells knex where to put the seeds folder 
 seeds: {
-directory:'./data/migration'
+directory:'./data/seeds'
 }
+  }
 };
